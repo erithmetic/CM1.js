a8373a666a9d83b72b2640052abea74c577cc33f
Derek Kastner <dkastner@gmail.com>
2011-03-11 10:22:30 -0500

    Added semicolons for minifcationability

--- spec/CarbonSpec.js
+++ spec/CarbonSpec.js
@@ -1,9 +1,9 @@
-RentalCar = function() {}
+RentalCar = function() {};
 Carbon.emitter(RentalCar, function(emitter) {
-  emitter.emitAs('automobile')
-  emitter.provide('make')
-  emitter.provide('model')
-  emitter.provide('fuel_economy', { as: 'fuel_efficiency' })
+  emitter.emitAs('automobile');
+  emitter.provide('make');
+  emitter.provide('model');
+  emitter.provide('fuel_economy', { as: 'fuel_efficiency' });
 })
 
 describe('Carbon.js usage', function() {
@@ -12,163 +12,163 @@ describe('Carbon.js usage', function() {
       'http://carbon.brighterplanet.com/automobiles.json?make=Honda&model=Fit&fuel_economy=36.7': {
         successData: Cm1Result.fit  
       }
-    } })
+    } });
 
-    var car = new RentalCar()
-    car.make = 'Honda'
-    car.model = 'Fit'
-    car.fuel_efficiency = 36.7
+    var car = new RentalCar();
+    car.make = 'Honda';
+    car.model = 'Fit';
+    car.fuel_efficiency = 36.7;
 
-    var value
+    var value;
     car.getEmissionEstimate(function(estimate) {
-      value = estimate.value()
-    })
+      value = estimate.value();
+    });
 
-    expect(value).toBe(3563.616916486099)
-    expect(car.emissionEstimate.value()).toBe(3563.616916486099)
-  })
-})
+    expect(value).toBe(3563.616916486099);
+    expect(car.emissionEstimate.value()).toBe(3563.616916486099);
+  });
+});
 
 describe('Carbon', function() {
-  var carbon
+  var carbon;
 
   beforeEach(function() {
-    carbon = new Carbon()
-  })
+    carbon = new Carbon();
+  });
 
   describe('.emitter', function() {
-    var car
+    var car;
     beforeEach(function() {
-      car = new RentalCar
-    })
+      car = new RentalCar;
+    });
 
     it('sets the type of emitter', function() {
-      expect(RentalCar.carbon.emitter_name).toBe('automobile')
-    })
+      expect(RentalCar.carbon.emitter_name).toBe('automobile');
+    });
     it('creates an #emissionEstimate method on the target class', function() {
-      expect(car.emissionEstimate).toBeInstanceOf(EmissionEstimate)
-    })
+      expect(car.emissionEstimate).toBeInstanceOf(EmissionEstimate);
+    });
     it('creates an #emissionEstimator method on the target class', function() {
-      expect(car.emissionEstimator()).toBeInstanceOf(EmissionEstimator)
-    })
+      expect(car.emissionEstimator()).toBeInstanceOf(EmissionEstimator);
+    });
     it('creates a #getEmissionEstimate method on the target class', function() {
-      expect(car.getEmissionEstimate()).not.toBe(null)
-    })
+      expect(car.getEmissionEstimate()).not.toBe(null);
+    });
   })
 
   describe('#define', function() {
     it('runs the specified methods on the Carbon instance', function() {
       carbon.define(function(instance) {
-        instance.emitAs('gyrocopter')
-      })
-      expect(carbon.emitter_name).toBe('gyrocopter')
+        instance.emitAs('gyrocopter');
+      });
+      expect(carbon.emitter_name).toBe('gyrocopter');
     })
   })
 
   describe('#emitAs', function() {
     it('sets the emitter name', function() {
-      carbon.emitAs('dirigible')
-      expect(carbon.emitter_name).toBe('dirigible')
-    })
-  })
+      carbon.emitAs('dirigible');
+      expect(carbon.emitter_name).toBe('dirigible');
+    });
+  });
 
   describe('#provide', function() {
     it('maps an emitter attribute with the same name as a class attribute', function() {
-      carbon.provide('air_speed')
-      expect(carbon.attribute_map['air_speed']).toBe('air_speed')
+      carbon.provide('air_speed');
+      expect(carbon.attribute_map['air_speed']).toBe('air_speed');
     })
     it('maps an emitter attribute with a different name than a class attribute', function() {
-      carbon.provide('air_speed', { as: 'velocity' })
-      expect(carbon.attribute_map['air_speed']).toBe('velocity')
-    })
-  })
-})
+      carbon.provide('air_speed', { as: 'velocity' });
+      expect(carbon.attribute_map['air_speed']).toBe('velocity');
+    });
+  });
+});
 
 
 
 describe('EmissionEstimator', function() {
-  var car, estimator
+  var car, estimator;
 
   beforeEach(function() {
-    car = new RentalCar()
-    car.make = 'Honda'
-    car.model = 'Fit'
-    car.fuel_efficiency = 38.2
+    car = new RentalCar();
+    car.make = 'Honda';
+    car.model = 'Fit';
+    car.fuel_efficiency = 38.2;
 
-    estimator = new EmissionEstimator(car, RentalCar.carbon)
+    estimator = new EmissionEstimator(car, RentalCar.carbon);
   })
 
   describe('#url', function() {
     it('returns the proper url for the emitter', function() {
-      expect(estimator.url()).toBe('http://carbon.brighterplanet.com/automobiles.json')
-    })
-  })
+      expect(estimator.url()).toBe('http://carbon.brighterplanet.com/automobiles.json');
+    });
+  });
 
   describe('#params', function() {
     it('returns an empty object if no params are set', function() {
-      car.make = null
-      car.model = null
-      car.fuel_efficiency = null
-      expect(estimator.params()).toEqual({})
-    })
+      car.make = null;
+      car.model = null;
+      car.fuel_efficiency = null;
+      expect(estimator.params()).toEqual({});
+    });
     it('returns an object mapping CM1 params to emitter attribute values', function() {
       expect(estimator.params()).toEqual({
         make: 'Honda',
         model: 'Fit',
         fuel_economy: 38.2
-      })
-    })
-  })
+      });
+    });
+  });
 
   describe('#getEmissionEstimate', function() {
-    var onSuccess, onError
+    var onSuccess, onError;
 
     beforeEach(function() {
       fakeAjax({ urls: {
         'http://carbon.brighterplanet.com/automobiles.json?make=Honda&model=Fit&fuel_economy=38.2': {
           successData: Cm1Result.fit  
         }
-      } })
+      } });
 
-      onSuccess = jasmine.createSpy('onSuccess')
-      onError = jasmine.createSpy('onError')
-    })
+      onSuccess = jasmine.createSpy('onSuccess');
+      onError = jasmine.createSpy('onError');
+    });
 
     it('calls the given onSuccess method with the emissionEstimate', function() {
-      car.getEmissionEstimate(onSuccess, onError)
-      expect(onSuccess).toHaveBeenCalledWith(car.emissionEstimate)
-    })
+      car.getEmissionEstimate(onSuccess, onError);
+      expect(onSuccess).toHaveBeenCalledWith(car.emissionEstimate);
+    });
     it("sets the data attribute on the emitter's EmissionEstimate", function() {
-      car.getEmissionEstimate(onSuccess, onError)
-      expect(car.emissionEstimate.data).toBe(Cm1Result.fit)
-    })
-  })
-})
+      car.getEmissionEstimate(onSuccess, onError);
+      expect(car.emissionEstimate.data).toBe(Cm1Result.fit);
+    });
+  });
+});
 
 
 describe('EmissionEstimate', function() {
-  var estimate
+  var estimate;
 
   beforeEach(function() {
-    estimate = new EmissionEstimate()
-    estimate.data = Cm1Result.fit
-  })
+    estimate = new EmissionEstimate();
+    estimate.data = Cm1Result.fit;
+  });
   
   describe('#value', function() {
     it('returns the emission value', function() {
-      expect(estimate.value()).toBe(3563.616916486099)
-    })
-  })
+      expect(estimate.value()).toBe(3563.616916486099);
+    });
+  });
 
   describe('#methodology', function() {
     it('returns the methodology URL', function() {
-      expect(estimate.methodology()).toBe('http://carbon.brighterplanet.com/automobiles.html?make=Honda&timeframe=2011-01-01%2F2012-01-01')
-    })
-  })
+      expect(estimate.methodology()).toBe('http://carbon.brighterplanet.com/automobiles.html?make=Honda&timeframe=2011-01-01%2F2012-01-01');
+    });
+  });
 
   describe('#toString', function() {
     it('returns a string representation of the emission value', function() {
-      expect(estimate.toString()).toBe('3563.616916486099')
-    })
-  })
-})
+      expect(estimate.toString()).toBe('3563.616916486099');
+    });
+  });
+});
